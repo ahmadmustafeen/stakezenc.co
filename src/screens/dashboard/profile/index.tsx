482f6790503c311e64f78df273d928ca4e92097f
Ahmad Mustafeen <engr.ahmadmustafeen@gmail.com>
2023-05-05 14:05:07 +0500

    FIX: profile screen code updation

--- src/screens/dashboard/profile/index.tsx
+++ src/screens/dashboard/profile/index.tsx
@@ -2,7 +2,6 @@ import {Image, ScrollView, View} from 'react-native';
 import React from 'react';
 import {styles} from './style';
 import {AppText, Header} from '../../../components';
-import {bgPrimary, lighterBackground, primaryBlue} from '../../../constants';
 
 const ProfileScreen = () => {
   return (
@@ -11,86 +10,34 @@ const ProfileScreen = () => {
 
       <ScrollView>
         <View style={styles.profileContainer}>
-          <View
-            style={{
-              height: 150,
-              justifyContent: 'center',
-              alignItems: 'center',
-            }}>
+          <View style={styles.imageContainer}>
             <Image
               source={require('../../../assets/images/user-profile.png')}
-              style={{
-                width: 100,
-                height: 100,
-                borderWidth: 2,
-                borderRadius: 100,
-                borderColor: primaryBlue,
-              }}
+              style={styles.image}
             />
           </View>
           <AppText children="Ahmad Mustafeen" center white heading />
-          <View
-            style={{
-              marginVertical: 10,
-              width: '90%',
-              alignSelf: 'center',
-              paddingVertical: 20,
-            }}>
-            <View
-              style={{
-                paddingHorizontal: 10,
-                backgroundColor: lighterBackground,
-                marginVertical: 1,
-                paddingVertical: 5,
-                flexDirection: 'row',
-                justifyContent: 'space-between',
-              }}>
+          <View style={styles.infoContainer}>
+            <View style={styles.info}>
               <AppText white>Account status</AppText>
-              <View
-                style={{padding: 5, borderRadius: 5, backgroundColor: 'red'}}>
+              <View style={styles.statusContainer}>
                 <AppText white>Account status</AppText>
               </View>
             </View>
-            <View
-              style={{
-                paddingHorizontal: 10,
-                backgroundColor: lighterBackground,
-                marginVertical: 1,
-                paddingVertical: 5,
-                flexDirection: 'row',
-                justifyContent: 'space-between',
-              }}>
+            <View style={styles.info}>
               <AppText white>Account status</AppText>
-              <View
-                style={{padding: 5, borderRadius: 5, backgroundColor: 'red'}}>
+              <View style={styles.statusContainer}>
                 <AppText white>Account status</AppText>
               </View>
             </View>
 
-            <View
-              style={{
-                paddingHorizontal: 10,
-                backgroundColor: lighterBackground,
-                marginVertical: 1,
-                paddingVertical: 5,
-                flexDirection: 'row',
-                justifyContent: 'space-between',
-              }}>
+            <View style={styles.info}>
               <AppText white>Account status</AppText>
-              <View
-                style={{padding: 5, borderRadius: 5, backgroundColor: 'red'}}>
+              <View style={styles.statusContainer}>
                 <AppText white>Account status</AppText>
               </View>
             </View>
-            <View
-              style={{
-                paddingHorizontal: 10,
-                paddingVertical: 5,
-                backgroundColor: lighterBackground,
-                marginVertical: 1,
-                flexDirection: 'row',
-                justifyContent: 'space-between',
-              }}>
+            <View style={styles.info}>
               <AppText white>Account status</AppText>
               <AppText text white>
                 $ 0
--- src/screens/dashboard/profile/style.ts
+++ src/screens/dashboard/profile/style.ts
@@ -1,5 +1,10 @@
 import {StyleSheet} from 'react-native';
-import {darkerPrimary, headerBlue} from '../../../constants';
+import {
+  darkerPrimary,
+  headerBlue,
+  lighterBackground,
+  primaryBlue,
+} from '../../../constants';
 
 export const styles = StyleSheet.create({
   container: {
@@ -17,5 +22,36 @@ export const styles = StyleSheet.create({
     borderRadius: 5,
     borderWidth: 0.5,
     borderStyle: 'solid',
+  },
+  imageContainer: {
+    height: 150,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image: {
+    width: 100,
+    height: 100,
+    borderWidth: 2,
+    borderRadius: 100,
+    borderColor: primaryBlue,
+  },
+  infoContainer: {
+    marginVertical: 10,
+    width: '90%',
+    alignSelf: 'center',
+    paddingVertical: 20,
+  },
+  info: {
+    paddingHorizontal: 10,
+    backgroundColor: lighterBackground,
+    marginVertical: 1,
+    paddingVertical: 5,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  statusContainer: {
+    padding: 5,
+    borderRadius: 5,
+    backgroundColor: 'red',
   },
 });
